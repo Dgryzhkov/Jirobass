d986d698dc050062f1614e1e03da736dc3689cbf
Denis Ryzhkov <dgryzhkov@gmail.com>
2023-01-08 15:15:29 +0700

    add first fragmnt

--- app/src/main/java/com/example/jirobass/MainActivity.kt
+++ app/src/main/java/com/example/jirobass/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.jirobass
 
-import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.jirobass.fragments.DaysFragment
+import com.example.jirobass.utils.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FragmentManager.setFragment(DaysFragment.newInstance(), this)
     }
 }
